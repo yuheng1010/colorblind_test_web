ff862dba25cd6a1391d86f0e225abdf363e299d5
Doris419 <doris32103210d@gmail.com>
2023-04-02 20:48:54 +0800

    qrcord

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,11 +1,11 @@
 import React, { useDebugValue, useEffect, useContext, useState, useSyncExternalStore } from 'react';
-import { BrowserRouter, Switch, Route, Routes, Link, NavLink, Redirect } from "react-router-dom";
+// import { BrowserRouter, Switch, Route, Routes, Link, NavLink, Redirect } from "react-router-dom";
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-
-import TestArea from './components/testArea'
+import TestArea from './components/testArea';
 import InsertData from './components/insertData';
 // import Button from 'next/Button'
 
@@ -16,6 +16,7 @@ export default function Home() {
   const [posX, setposX] = React.useState(0);
   const [posY, setposY] = React.useState(0);
   const [tru, setTru] = React.useState(true);
+
 
   return (
     <>
