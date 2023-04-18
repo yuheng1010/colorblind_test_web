f81e61a8e98564dcfe55d3ebc97cedb9e8f03b02
Heng <henhen90@gmail.com>
2023-04-18 16:41:26 +0800

    0418

--- front/pages/qrcord/[qrcordId].js
+++ front/pages/qrcord/[qrcordId].js
@@ -2,17 +2,50 @@ import React, { useState, useEffect } from 'react'
 import { useQRCode } from 'next-qrcode';
 import { useRouter } from 'next/router'
 import QRCode from "qrcode.react";
-
+import { Helmet } from 'react-helmet';
+import { saveAs } from 'file-saver';
 function QRCORD() {
     // const { QRCORD } = useQRCode();
     const router = useRouter()
-    const qrcordId = router.query.qrcordId
+    const qrcodeId = router.query.qrcordId
 
+    let level = ""
+    let type = ""
+    if (qrcodeId) {
+        if (qrcodeId[0] == "B") {
+            type = "protanomalous"
+        } else if (qrcodeId[0] == "C") {
+            type = "deuteranomalous"
+        }
+        if (qrcodeId[1] == 1) {
+            level = "severe"
+        } else if (qrcodeId[1] == 2) {
+            level = "moderate"
+        } else if (qrcodeId[1] == 3) {
+            level = "mild"
+        }
+    }
+    function Download() {
+        const img = document.getElementById('qr');
+        img.toBlob((blob) => {
+            saveAs(blob, 'qrcode.png');
+        });
+    }
     return (
-        <>
-            <div>{qrcordId}</div>
-            
-            <QRCode value={qrcordId}
+
+        <div className='resultPage'>
+            <Helmet>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.0/FileSaver.min.js"></script>
+            </Helmet>
+            <div className='resTitle'>Colorblind Test Results</div>
+            <div className='textArea'>
+                <div className='resType'>Type : {type} </div>
+                <div className='resLev'>Level : {level} </div>
+            </div>
+
+            {/* <div>{qrcodeId}</div> */}
+
+            <QRCode id="qr" className="qr" value={qrcodeId}
                 options={{
                     level: 'M',
                     margin: 3,
@@ -23,21 +56,10 @@ function QRCORD() {
                         light: '#ffffff',
                     },
                 }} />
-
-            {/* <QRCORD
-                text={'https://github.com/bunlong/next-qrcode'}
-                options={{
-                    level: 'M',
-                    margin: 3,
-                    scale: 4,
-                    width: 200,
-                    color: {
-                        dark: '#000000',
-                        light: '#ffffff',
-                    },
-                }}
-            /> */}
-        </>
+            <div className='ins'>Now scan this QRcode on the Hololens.</div>
+            <br />
+            <button className='dLBtn' onClick={Download}>Download QRcode</button>
+        </div>
     )
 
 
